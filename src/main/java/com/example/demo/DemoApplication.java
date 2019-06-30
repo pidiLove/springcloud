0d7a78879080954d7f3cac72a863545fcc331be1
suiyuehanhao <1278808836@qq.com>
2019-06-30 19:07:23 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+// public class Application {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(Application.class, args);
+//    }
+//
+//     public String getValue() {
+//
+//        return "liming";
+//
+//    }
+//}
